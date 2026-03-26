034ba6fd8de05e1ebefc765191ecc3baf472fffe
David <aplswrk3.0@gmail.com>
2026-03-26 14:59:27 -0500

    chore: configure environment variables

--- database/db.js
+++ database/db.js
@@ -1,11 +1,11 @@
 const { Pool } = require("pg");
 
 const pool = new Pool({
- user: "nuevo",
- host: "localhost",
- database: "nutriedu",
- password: "1234",
- port: 5432,
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: Number(process.env.DB_PORT),
 });
 
 module.exports = pool;
