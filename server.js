bfed864efa311030e32f6c14c557a7ef7bcb093a
David <aplswrk3.0@gmail.com>
2026-04-13 23:47:59 -0500

    v2

--- server.js
+++ server.js
@@ -1,20 +1,20 @@
 require("dotenv").config();
+
 const express = require("express");
 const cors = require("cors");
 
 const userRoutes = require("./routes/userRoutes");
 const recipeRoutes = require("./routes/recipeRoutes");
 
-
 const app = express();
+const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
+
 app.use("/api/recipes", recipeRoutes);
 app.use("/api/users", userRoutes);
 
-const PORT = process.env.PORT || 3000;
-
 app.listen(PORT, () => {
- console.log("Servidor corriendo en puerto", PORT);
+  console.log(`Servidor corriendo en puerto ${PORT}`);
 });
