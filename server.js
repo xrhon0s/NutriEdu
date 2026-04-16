b187309eb4c4645859584ece05cba4f0e2314031
David <aplswrk3.0@gmail.com>
2026-04-15 23:43:16 -0500

    feat(planner): register planner routes in server

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const cors = require("cors");
 
 const userRoutes = require("./routes/userRoutes");
 const recipeRoutes = require("./routes/recipeRoutes");
+const plannerRoutes = require("./routes/plannerRoutes");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -14,6 +15,7 @@ app.use(express.json());
 
 app.use("/api/recipes", recipeRoutes);
 app.use("/api/users", userRoutes);
+app.use("/api/planner", plannerRoutes);
 
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en puerto ${PORT}`);
